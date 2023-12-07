60736575a057a085f2171f3ea6c69125bf67ce32
Lee-Seungje <moondgod@gmail.com>
2023-12-07 17:42:58 +0900

    Set

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import Item from "./components/UserItem";
+
+const userList = [
+  {
+    id: 0,
+    userName: "방가온",
+  },
+  {
+    id: 1,
+    userName: "신희성",
+  },
+  {
+    id: 2,
+    userName: "이승제",
+  },
+  {
+    id: 3,
+    userName: "전예빈",
+  },
+  {
+    id: 4,
+    userName: "하제우",
+  },
+  {
+    id: 5,
+    userName: "최장우",
+  },
+  {
+    id: 6,
+    userName: "김하온",
+  },
+  {
+    id: 7,
+    userName: "박준호",
+  },
+  {
+    id: 8,
+    userName: "김주은",
+  },
+  {
+    id: 9,
+    userName: "장예슬",
+  },
+];
 
 function App() {
+  const deleteUserInfo = () => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>test</h1>
     </div>
   );
 }
---
+++ src/components/UserItem.jsx
@@ -0,0 +1,10 @@
+const UserItem = () => {
+  return (
+    <div>
+      <span>유저 이름: {}</span>
+      <button>유저 정보 삭제</button>
+    </div>
+  );
+};
+
+export default UserItem;
