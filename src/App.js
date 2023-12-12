912dbab0e44fec7e35b8c22ac2d72cf8f746536b
gjaegyun <jaegyungim06@gmail.com>
2023-12-12 10:36:32 +0900

    Add userList Function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import Item from "./components/UserItem";
+import { useState } from "react";
+import UserItem from "./components/UserItem";
 
 const userList = [
   {
@@ -44,15 +45,35 @@ const userList = [
 ];
 
 function App() {
-  const createUserInfo = () => {};
-  const deleteUserInfo = () => {};
+  const [inputUser, setInputUser] = useState("");//user이름 받기 위한 state생성
+  const [userArr, setUserArr] = useState(userList);//초기값을 userList로 해줘서 userList를 기준으로 더 추가하게 list state생성
+
+  const createUserInfo = () => {
+    const newUser = {
+      id: userArr.length, //입력받은거 토대로 객체 생성
+      userName: inputUser,
+    };
+    setUserArr([...userArr, newUser]); //...userArr로 전에있던 배열유지, newUser로 새 객체 배열에 담기
+    setInputUser(""); //setUserArr로 array에 저장 한뒤에 빈 string으로 set해서 input 초기화
+  };
+
+  const deleteUserInfo = (userId) => {
+    setUserArr(userArr.filter((user) => user.id !== userId)); //props로 받은 userId와 다른 id만 filter()로 걸러 set해준다.
+  };
 
   return (
     <div>
       <h1>Hello, GSM 신규 입학 지원자 명단</h1>
       {/* 여기에 지원자 명단 표시 */}
-      <input placeholder="지원자의 이름을 입력해주세요" />
-      <button>지원자 추가</button>
+      {userArr.map((user) => 
+        <UserItem key={user.id} user={user} onDelete={() => deleteUserInfo(user.id)}></UserItem> //userItem 컴포넌트에 id와 user컴포넌트로 전달 및 delete에 id를 props로 전달 해줘서 정해진 id 삭제할수 있게 함
+      )}
+      <input 
+        value={inputUser} //value를 inputUser로 둬서 입력하는 값으로 둠
+        onChange={(e) => setInputUser(e.target.value)}//e.target.value를 통해서 사용자가 입력한 값을 setInputUser로 inputUser을 사용자가 입력한 user로 바꾸어줌
+        placeholder="지원자의 이름을 입력해주세요" 
+      />
+      <button onClick={createUserInfo}>지원자 추가</button>
     </div>
   );
 }
--- src/components/UserItem.jsx
+++ src/components/UserItem.jsx
@@ -1,8 +1,8 @@
-const UserItem = () => {
-  return (
+const UserItem = ({user, onDelete}) => { //props 받아오기
+  return (//받아온 props 기반으로 userName과 유저정보삭제 클릭시 onDelete함수 실행되게 구현
     <div>
-      <span>유저 이름: {}</span>
-      <button>유저 정보 삭제</button>
+      <span>유저 이름: {user.userName}</span>
+      <button onClick={onDelete}>유저 정보 삭제</button>
     </div>
   );
 };
